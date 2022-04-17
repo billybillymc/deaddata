2f311c2e0e89ef70b16dc41bdbdafe02c27547f7
billybillymc <mcintyre.w@gmail.com>
2022-04-17 16:36:13 -0700

    Add files via upload

---
+++ deadfiles.py
@@ -0,0 +1,35 @@
+import requests
+import os
+import json
+import re
+from urllib.parse import urlparse
+import time
+
+e = 'deaddata2/info_saves'
+x = 0
+for filename in os.listdir(e):
+    if '.json' in filename:
+        file = os.path.join(e, filename)
+        f = open(file)
+        data = json.load(f)
+        show = filename.replace('.json','')
+        print(show)
+        os.makedirs('E:/deaddata/' + show)
+        for g in data['link_of_songs']:
+            time.sleep(4.5)
+            a = urlparse(g)
+            v = os.path.basename(a.path)
+            doc = requests.get(g)
+            with open('E:/deaddata/' + show + '/' + v, 'wb') as f:
+                f.write(doc.content)
+                print(v)
+        print()
+        print()
+        print()
+        x += 1
+        print(show + ' is done.' + str(x) + '/16127')
+        print()
+        print()
+        print()
+
+## Several shows are empty becuase they're commercially available. Several are also appearances on talk shows.
